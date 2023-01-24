fcb2e11116604e39e9fd0c3d040cd62a35af7cf4
Omar MHAIMDAT <omarmhaimdat@gmail.com>
2023-01-24 20:46:25 +0100

    Add brat and conll format

--- src/models.rs
+++ src/models.rs
@@ -1,5 +1,5 @@
-use ::serde::{Deserialize, Serialize};
 use rayon::prelude::*;
+use serde::{Deserialize, Serialize};
 use std::{
     collections::{HashMap, HashSet},
     io::Write,
@@ -43,9 +43,20 @@ impl Format {
             Format::Spacy => Format::spacy(annotations, path),
             Format::Jsonl => Format::jsonl(annotations, path),
             Format::Csv => Format::csv(annotations, path),
+            Format::Brat => Format::brat(annotations, path),
+            Format::Conll => Format::conll(annotations, path),
             _ => unimplemented!(),
         }
     }
+
+    fn remove_extension_from_path(path: &str) -> String {
+        let mut path = path.to_string();
+        if path.contains(".") {
+            path.truncate(path.rfind(".").unwrap());
+        }
+        path
+    }
+
     fn spacy(annotations: Vec<Annotation>, path: &str) -> Result<(), std::io::Error> {
         // Save as such [["text", {"entity": [[0, 4, "ORG"], [5, 10, "ORG"]]}]]
         let mut file = std::fs::File::create(path)?;
@@ -82,6 +93,88 @@ impl Format {
         for annotation in annotations {
             let json = serde_json::to_string(&annotation).unwrap();
             file.write_all(json.as_bytes())?;
+            file.write_all(b"\n")?;
+        }
+        Ok(())
+    }
+
+    fn brat(annotations: Vec<Annotation>, path: &str) -> Result<(), std::io::Error> {
+        // Save .ann and .txt files
+        // .ann file
+        // T1	ORG 0 4	Apple
+        // T2	ORG 5 10	Inc
+        // .txt file
+        // Apple Inc
+        // Save as brat format
+        let mut file_ann = std::fs::File::create(format!("{}.ann", path))?;
+        let mut file_txt = std::fs::File::create(format!("{}.txt", path))?;
+        for annotation in annotations {
+            let text = annotation.text;
+            file_txt.write_all(text.as_bytes())?;
+            file_txt.write_all(b"\n")?;
+            let mut id = 0;
+            for (start, end, label) in annotation.label {
+                let entity = text[start..end].to_string();
+                let line = format!("T{}\t{}\t{}\t{}\t{}", id, label, start, end, entity);
+                file_ann.write_all(line.as_bytes())?;
+                file_ann.write_all(b"\n")?;
+                id += 1;
+            }
+        }
+        Ok(())
+    }
+
+    fn conll(annotations: Vec<Annotation>, path: &str) -> Result<(), std::io::Error> {
+        // for reference: https://simpletransformers.ai/docs/ner-data-formats/
+        // Example:
+        // Text = Harry Potter was a student at Hogwarts
+        // Harry B-PER
+        // Potter I-PER
+        // was O
+        // a O
+        // student B-MISC
+        // at B-PER
+        // Hogwarts I-PER
+        // Use the example above to save the data
+        let mut file = std::fs::File::create(path)?;
+        // Transform text to fit this structure
+        // [["Harry", "B-PER"], ["Potter", "I-PER"], ["was", "O"], ["a", "O"], ["student", "B-MISC"], ["at", "B-PER"], ["Hogwarts", "I-PER"]]
+        // Perform the same operation for each text in annotations
+        let annotations_tranformed: Vec<Vec<(String, String)>> = annotations
+            .into_iter()
+            .map(|annotation| {
+                let text = annotation.text;
+                // Split text into words
+                let words: Vec<&str> = text.split_whitespace().collect();
+                // If the word is not associated with an entity, then it is an "O"
+                let mut labels: Vec<String> = vec!["O".to_string(); words.len()];
+                // For each entity, find the word that contains it and assign the label to it
+                for (start, end, label) in annotation.label {
+                    let entity = text[start..end].to_string();
+                    // Find the index of the word that contains the entity
+                    let index = words.iter().position(|&word| word.contains(&entity));
+                    if let None = index {
+                        continue;
+                    }
+                    let index = index.unwrap();
+                    // If the word is the same as the entity, then it is a "B" label
+                    labels[index] = format!("{}", label);
+                }
+                // Combine the words and labels into a single vector
+                words
+                    .iter()
+                    .zip(labels.iter())
+                    .map(|(word, label)| (word.to_string(), label.to_string()))
+                    .collect()
+            })
+            .collect();
+        // Save the data, one line per word with the word and label separated by a space
+        for annotation in annotations_tranformed {
+            for (word, label) in annotation {
+                let line = format!("{}\t{}", word, label);
+                file.write_all(line.as_bytes())?;
+                file.write_all(b"\n")?;
+            }
             file.write_all(b"\n")?;
         }
         Ok(())
